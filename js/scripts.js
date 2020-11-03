338ebb122157cd36732d027379564a93b163cec0
Jacob D <35861176+JakeDLL@users.noreply.github.com>
2020-11-03 17:54:07 -0500

    Add user data and gallery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
-async function fetchUsers(url) {
-    const usersJSON = await fetch(url).then(response => response.json())
+const users = new Promise((resolve, reject) => {
+    const usersJSON = fetch("https://randomuser.me/api/?results=12").then(response => response.json());
+    resolve(usersJSON);
+});
 
-    return await usersJSON.results;
+async function createGallery(json) {
+    const gallery = document.querySelector('#gallery');
+    const users = await json.results;
+    users.forEach(user => {
+        console.log(user);
+        const card = `
+            <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${user.picture.large}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="${user.name.first}-${user.name.last}" class="card-name cap">${user.name.first} ${user.name.last}</h3> 
+                    <p class="card-text">${user.email}</p>
+                    <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+                </div>
+            </div>
+        `;
+        gallery.insertAdjacentHTML('beforeend', card);
+    });
 }
-console.log(fetchUsers('https://randomuser.me/api/?results=12'));
+users.then(createGallery);
 // info to get from api:
 /**
  * gallery:
